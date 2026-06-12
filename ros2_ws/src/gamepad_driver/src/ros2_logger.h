c2fde63fbbb74cfab95d35ddb275c30be4cb7792
Nico Schubert <nic.schubert@gmx.de>
2026-06-12 14:38:02 +0200

    fix wrong logger name of gamepad

--- ros2_ws/src/gamepad_driver/src/ros2_logger.h
+++ ros2_ws/src/gamepad_driver/src/ros2_logger.h
@@ -1,12 +1,12 @@
 #ifndef __ROS2_LOGGER_H__
 #define __ROS2_LOGGER_H__
 
-#ifdef USE_LOGGING_MOTOR_CONTROLLER
+#ifdef USE_LOGGING_GAME_PAD
     #undef USE_LOGGING
-    #define USE_LOGGING USE_LOGGING_MOTOR_CONTROLLER
+    #define USE_LOGGING USE_LOGGING_GAME_PAD
 #endif
 
-#define LOGGING_NAME "motor_controller"
+#define LOGGING_NAME "gamepad_driver"
 #include "volksface/logging.h"
 
 #endif
